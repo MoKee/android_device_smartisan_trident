1d69f01182fb7c4c88d1688e89c1287b6adaa8c3
XiNGRZ <chenxingyu92@gmail.com>
2019-04-26 21:02:15 +0800

    trident: mkbootimg: Fix building update zip
    Change-Id: Id75ea35f8ab929277649af4d5940d7bce3e3eecf

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -14,11 +14,24 @@ INTERNAL_RECOVERYIMAGE_ARGS += \
 $(INSTALLED_RECOVERY_RAMDISK_TARGET): $(BOARD_PREBUILT_RECOVERY_RAMDISK)
 	cp $(BOARD_PREBUILT_RECOVERY_RAMDISK) $@
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_FILES) $(RECOVERYIMAGE_EXTRA_DEPS)
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTFS) $(MKBOOTIMG) $(MINIGZIP) $(ADBD) \
+		$(INSTALLED_RAMDISK_TARGET) \
+		$(INTERNAL_RECOVERYIMAGE_FILES) \
+		$(recovery_initrc) $(recovery_sepolicy) $(recovery_kernel) \
+		$(INSTALLED_2NDBOOTLOADER_TARGET) \
+		$(recovery_build_props) $(recovery_resource_deps) \
+		$(recovery_fstab) \
+		$(RECOVERY_INSTALL_OTA_KEYS) \
+		$(INSTALLED_VENDOR_DEFAULT_PROP_TARGET) \
+		$(BOARD_RECOVERY_KERNEL_MODULES) \
+		$(DEPMOD)
 	$(call pretty,"Target boot image from recovery: $@")
+	$(call build-recoveryramdisk)
 	$(hide) $(MKBOOTIMG) \
 		$(INTERNAL_RECOVERYIMAGE_ARGS) \
 		$(INTERNAL_MKBOOTIMG_VERSION_ARGS) \
+		$(BOARD_MKBOOTIMG_ARGS) \
 		--output $@ \
 		--id > $(RECOVERYIMAGE_ID_FILE)
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
+	@echo ----- Made recovery image: $@ --------
