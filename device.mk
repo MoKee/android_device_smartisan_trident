b2266b4dc6f2b848239dd2935b94af561afc075d
XiNGRZ <chenxingyu92@gmail.com>
2019-08-16 22:56:31 +0800

    trident: Build audio HAL from source
    This reverts commit aa7b7feefa15475e932dd51e863e2e21999b1476.
    
    Change-Id: I4534760ffc827af9a81667ddf6942928ef39171d

--- device.mk
+++ device.mk
@@ -128,11 +128,14 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.soundtrigger@2.1-impl \
+    audio.primary.sdm845 \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
+    libaudioroute \
     libtinyalsa \
+    libtinycompress \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
