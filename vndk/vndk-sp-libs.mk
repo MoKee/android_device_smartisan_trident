fbdc57ad1eac2446ffb7a88eda6548644b2c74f1
XiNGRZ <chenxingyu92@gmail.com>
2019-08-20 17:30:05 +0800

    trident: Add more libs to vndk-sp
    Change-Id: Ia03190a77182487303eca4daa0b67f1f1b815ac1

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -31,9 +31,13 @@ VNDK_SP_LIBRARIES := \
 
 EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
+    com.qualcomm.qti.imscmservice@2.0 \
+    com.qualcomm.qti.uceservice@2.0 \
     vendor.display.config@1.0 \
     vendor.display.config@1.1 \
     vendor.mokee.livedisplay@2.0 \
     vendor.mokee.power@1.0 \
     vendor.mokee.touch@1.0 \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.nxp.hardware.nfc@1.0 \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.latency@2.0
