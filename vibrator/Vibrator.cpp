a278f8513978d2f2ccb62d4ee9d2f35bf22bff21
XiNGRZ <chenxingyu92@gmail.com>
2019-10-07 19:18:04 +0800

    trident: Implemented vibrator@1.3 service
    Change-Id: Ic39da4eef6e4d3f6254434adf7f2fa092741c22d

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -24,15 +24,19 @@
 namespace android {
 namespace hardware {
 namespace vibrator {
-namespace V1_0 {
+namespace V1_3 {
 namespace implementation {
 
 Vibrator::Vibrator(std::ofstream&& vibrator_state,
                    std::ofstream&& vibrator_duration,
-                   std::ofstream&& vibrator_activate)
+                   std::ofstream&& vibrator_activate,
+                   std::ofstream&& vibrator_effect_name,
+                   std::ifstream&& vibrator_effect_play)
     : mVibratorState(std::move(vibrator_state))
     , mVibratorDuration(std::move(vibrator_duration))
     , mVibratorActivate(std::move(vibrator_activate))
+    , mVibratorEffectName(std::move(vibrator_effect_name))
+    , mVibratorEffectPlay(std::move(vibrator_effect_play))
     {
 }
 
@@ -68,13 +72,127 @@ Return<Status> Vibrator::setAmplitude(uint8_t) {
     return Status::UNSUPPORTED_OPERATION;
 }
 
-Return<void> Vibrator::perform(V1_0::Effect, EffectStrength, perform_cb _hidl_cb) {
-    _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
+Return<void> Vibrator::perform(V1_0::Effect effect, EffectStrength strength, perform_cb _hidl_cb) {
+    return perform<decltype(effect)>(effect, strength, _hidl_cb);
+}
+
+// Methods from ::android::hardware::vibrator::V1_1::IVibrator follow.
+
+Return<void> Vibrator::perform_1_1(V1_1::Effect_1_1 effect, EffectStrength strength,
+                                   perform_cb _hidl_cb) {
+    return perform<decltype(effect)>(effect, strength, _hidl_cb);
+}
+
+// Methods from ::android::hardware::vibrator::V1_2::IVibrator follow.
+
+Return<void> Vibrator::perform_1_2(V1_2::Effect effect, EffectStrength strength,
+                                   perform_cb _hidl_cb) {
+    return perform<decltype(effect)>(effect, strength, _hidl_cb);
+}
+
+// Methods from ::android::hardware::vibrator::V1_3::IVibrator follow.
+
+Return<bool> Vibrator::supportsExternalControl() {
+    return false;
+}
+
+Return<Status> Vibrator::setExternalControl(bool) {
+    return Status::UNSUPPORTED_OPERATION;
+}
+
+Return<void> Vibrator::perform_1_3(Effect effect, EffectStrength strength, perform_cb _hidl_cb) {
+    return perform<decltype(effect)>(effect, strength, _hidl_cb);
+}
+
+// Private methods follow.
+
+Return<void> Vibrator::perform(Effect effect, EffectStrength, perform_cb _hidl_cb) {
+    std::string phe;
+    std::string tmp;
+    uint32_t ms;
+    Status status = Status::OK;
+
+    if (!mVibratorEffectName || !mVibratorEffectPlay) {
+        status = Status::UNKNOWN_ERROR;
+        _hidl_cb(status, 0);
+        return Void();
+    }
+
+    ALOGI("Perform: Effect %s\n", effectToName(effect).c_str());
+
+    phe = effectToPheName(effect, &status);
+    if (status != Status::OK) {
+        _hidl_cb(status, 0);
+        return Void();
+    }
+
+    ms = effectToMs(effect);
+
+    mVibratorEffectName << phe << std::endl;
+    mVibratorEffectPlay >> tmp;
+
+    _hidl_cb(status, ms);
+
     return Void();
 }
 
+template <typename T>
+Return<void> Vibrator::perform(T effect, EffectStrength strength, perform_cb _hidl_cb) {
+    auto validRange = hidl_enum_range<T>();
+    if (effect < *validRange.begin() || effect > *std::prev(validRange.end())) {
+        _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
+        return Void();
+    }
+    return perform(static_cast<Effect>(effect), strength, _hidl_cb);
+}
+
+const std::string Vibrator::effectToName(Effect effect) {
+    return toString(effect);
+}
+
+const std::string Vibrator::effectToPheName(Effect effect, Status* status) {
+    switch (effect) {
+        case Effect::CLICK:
+            return "Keyboard";
+        case Effect::DOUBLE_CLICK:
+            return "VibMode";
+        case Effect::TICK:
+        case Effect::TEXTURE_TICK:
+            return "TimePicker";
+        case Effect::THUD:
+            return "ForceTouch";
+        case Effect::POP:
+            return "BigBang";
+        case Effect::HEAVY_CLICK:
+            return "Pin";
+        default:
+            *status = Status::UNSUPPORTED_OPERATION;
+            return "";
+    }
+}
+
+uint32_t Vibrator::effectToMs(Effect effect) {
+    switch (effect) {
+        case Effect::CLICK:
+            return 8;
+        case Effect::DOUBLE_CLICK:
+            return 212;
+        case Effect::TICK:
+        case Effect::TEXTURE_TICK:
+            return 2;
+        case Effect::THUD:
+            return 30;
+        case Effect::POP:
+            return 16;
+        case Effect::HEAVY_CLICK:
+            return 15;
+        default:
+            return 0;
+    }
+}
+
 }  // namespace implementation
-}  // namespace V1_0
+}  // namespace V1_3
 }  // namespace vibrator
 }  // namespace hardware
 }  // namespace android
