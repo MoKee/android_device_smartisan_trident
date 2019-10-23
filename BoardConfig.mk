6692d32648f660bd7e165fbd70c535950220a509
XiNGRZ <chenxingyu92@gmail.com>
2019-10-24 02:44:06 +0800

    trident: Remove A/B update and always mount B partition
    Change-Id: I2e1629780f2c385d6d7189f60ce2b90f01a4133e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,16 +65,6 @@ NEED_KERNEL_MODULE_SYSTEM := true
 # HAX: SELinux Permissive - Remove ASAP
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
-# A/B
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    vendor \
-    vbmeta
-
 # Audio
 USE_DEVICE_SPECIFIC_AUDIO := true
 USE_XML_AUDIO_POLICY_CONF := 1
@@ -148,15 +138,12 @@ BOARD_USES_QCOM_HARDWARE := true
 BUILD_WITHOUT_VENDOR := true
 
 # Recovery
-BOARD_USES_RECOVERY_AS_BOOT := true
-TARGET_NO_RECOVERY := true
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
 
 # Root
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_ROOT_EXTRA_FOLDERS := persist
 BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/dsp:/dsp \
